3511182f33f24f0cc820c64ba825f6d56e0ef801
DmutroV <voitovuch855@gmail.com>
2023-02-27 22:40:19 +0200

    task3

--- d_my_app/src/App.js
+++ d_my_app/src/App.js
@@ -1,10 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
+import ProductList from './Components/ProductList/ProductList';
 
 function App() {
-  return (
+
+const [products, setProducts] = useState([
+{id: 1, title: 'Product 1', price: 899 }, 
+{id: 2, title: 'Product 2', price: 427 },
+{id: 3, title: 'Product 3', price: 876 },
+{id: 4, title: 'Product 4', price: 563 },
+{id: 5, title: 'Product 5', price: 233 }
+
+]); 
+
+const deleteProduct = (productId) => {
+    const newProducts = products.filter(product => product.id !== productId);
+    setProducts (newProducts);
+}
+
+return (
     <div className="App">
       <Header/>
+      <ProductList products = {products} deleteProduct = {deleteProduct}/>
+
     </div>
   );
 }
--- d_my_app/src/Components/Header/Header.jsx
+++ d_my_app/src/Components/Header/Header.jsx
@@ -1,12 +1,56 @@
+import React, {useState} from "react";
+import logo  from "./logo2.png"
 import "./Header.scss"
-
+//хук (скритий ф-ціонал кнопи)
 const Header = () => {
-    return (
-        <div>
-            Header
-        </div>
+    const [showMenu, setShowMenu] = useState (false);
+    const [title, setTitel] = useState ("Is logged");
+    const nandleMenuToggle = () => {
+        setShowMenu (!showMenu);
+    }
+//хук (скритий ф-ціонал кнопи)
 
-    )
+// хук (для login)
+// const clickMe = (login) => {
+//     console.log ('Cliked' + login)
+// }
+// хук (для login)
+
+const changeTitel = () => {
+    setTitel("Relogin");
+}
+return (
+    <div>
+        <header className="header">
+            <div className="header-container">
+                <div className="logo-container">
+                    <img src={logo} alt = "logo" className="logo" />
+                </div>
+
+
+                    <nav className= {showMenu ? "nav nav-show" : "nav "}>
+                        <ul className="nav-list">
+                            <li className="nav-item">Home</li>
+                            <li className="nav-item">Shop</li>
+                            <li className="nav-item">About</li>
+                            <li className="nav-item">Contact</li>
+                        </ul>
+                    </nav>
+
+
+                    <div className="login-container">
+                        <button className="login-button" onClick={ () => changeTitel ()}>Login</button>
+                    </div>
+                        <div className="burger-container" onClick={nandleMenuToggle}>
+                        <div className="burger-line"></div>
+                        <div className="burger-line"></div>
+                        <div className="burger-line"></div>
+                </div>
+            </div>
+        </header>
+        <h3>{title}</h3>
+    </div>
+)
 }
 
 export default Header;
